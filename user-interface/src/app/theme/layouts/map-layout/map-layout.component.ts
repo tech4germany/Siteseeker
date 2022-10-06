1ef7e9e9c48e3f1c2a592d143213765e3c9caa46
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-10-06 10:01:54 +0200

    Add layouts

---
+++ user-interface/src/app/theme/layouts/map-layout/map-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-layout',
+  templateUrl: './map-layout.component.html',
+  styleUrls: ['./map-layout.component.scss']
+})
+export class MapLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
