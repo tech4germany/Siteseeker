8e86aa79ef64f6e9e39bc51d45410040cc11d3df
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-10-06 10:12:36 +0200

    Restructure child routing to use layouts

--- user-interface/src/app/content/content-routing.module.ts
+++ user-interface/src/app/content/content-routing.module.ts
@@ -4,11 +4,24 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { TestMapComponent } from './test-map/test-map.component';
 import { MapComponent } from './map/map.component';
+import { BaseLayoutComponent } from '../theme/layouts/base-layout/base-layout.component';
+import { MapLayoutComponent } from '../theme/layouts/map-layout/map-layout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'landingpage', pathMatch: 'full' },
-  { path: 'landingpage', component: LandingPageComponent },
-  { path: 'testmap', component: MapComponent },
+  { path: '', redirectTo: 'landingspace', pathMatch: 'full' },
+  {
+    path: 'landingspace',
+    component: BaseLayoutComponent,
+    children: [{ path: '', component: LandingPageComponent }],
+  },
+  {
+    path: 'userspace',
+    component: MapLayoutComponent,
+    children: [
+      { path: 'map', component: MapComponent },
+      { path: '', redirectTo: 'map', pathMatch: 'full' },
+    ],
+  },
   { path: '**', component: PageNotFoundComponent },
 ];
 
