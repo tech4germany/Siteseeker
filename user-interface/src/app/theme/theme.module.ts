b9f8dcb657abf9fcdcf167c952fb86ba890721f4
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-10-06 10:02:26 +0200

    Register new theme components in module

--- user-interface/src/app/theme/theme.module.ts
+++ user-interface/src/app/theme/theme.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { MapLayoutComponent } from './layouts/map-layout/map-layout.component';
+import { MapHeaderComponent } from './components/map-header/map-header.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, BaseLayoutComponent, MapLayoutComponent, MapHeaderComponent],
   imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, FooterComponent],
 })
