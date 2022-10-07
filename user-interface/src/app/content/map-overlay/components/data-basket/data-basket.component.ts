15ea3835b42c98539d53b90dc05d183f7405deaf
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-10-07 14:18:36 +0200

    Create Data Basket component

---
+++ user-interface/src/app/content/map-overlay/components/data-basket/data-basket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-basket',
+  templateUrl: './data-basket.component.html',
+  styleUrls: ['./data-basket.component.scss']
+})
+export class DataBasketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
