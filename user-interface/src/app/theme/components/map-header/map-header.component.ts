0409f4ad022c5af4ad96931d169c684f70876e95
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-10-07 14:22:19 +0200

    Rework map ui

--- user-interface/src/app/theme/components/map-header/map-header.component.ts
+++ user-interface/src/app/theme/components/map-header/map-header.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { MapService } from '../../../core/services/map.service';
+import { Coordinate } from 'ol/coordinate';
+import { GeocodingService } from '../../../core/services/geocoding.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-map-header',
   templateUrl: './map-header.component.html',
-  styleUrls: ['./map-header.component.scss']
+  styleUrls: ['./map-header.component.scss'],
 })
 export class MapHeaderComponent implements OnInit {
+  public coordinate: Coordinate = [0, 0];
+  public radius: number = 0;
+  public cityName: string = '';
+  public state: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private mapService: MapService,
+    private geocodingService: GeocodingService,
+    private router: Router
+  ) {
+    this.mapService
+      .getInputCoordinate()
+      .subscribe((coordinate: Coordinate) => (this.coordinate = coordinate));
+    this.mapService
+      .getRadius()
+      .subscribe((radius: number) => (this.radius = radius));
+    this.geocodingService
+      .getCityName()
+      .subscribe((name: string) => (this.cityName = name));
+    this.geocodingService
+      .getStateName()
+      .subscribe((name: string) => (this.state = name));
   }
 
+  ngOnInit(): void {}
+
+  public shortenDecimal(num: number) {
+    return Math.round(num * 100000) / 100000;
+  }
+
+  public navigateToLandingpage() {
+    this.router.navigateByUrl('/landingspace');
+  }
 }
