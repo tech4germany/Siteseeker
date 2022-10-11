30fed80413a8f1cbdfa5e4e633184c80b44ff3cf
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-10-11 09:45:33 +0200

    Add reverse geocoding

--- user-interface/src/app/core/services/geocoding.service.ts
+++ user-interface/src/app/core/services/geocoding.service.ts
@@ -3,16 +3,47 @@ import { Coordinate } from 'ol/coordinate';
 import { BehaviorSubject } from 'rxjs';
 import { MapService } from './map.service';
 
+import { reverseGeocode } from '@esri/arcgis-rest-geocoding';
+import { ApiKeyManager } from '@esri/arcgis-rest-request';
+import { environment } from '../../../environments/environment';
+import { Address } from '../models/Address';
+
 @Injectable({
   providedIn: 'root',
 })
 export class GeocodingService {
   cityName$: BehaviorSubject<string> = new BehaviorSubject('');
   state$: BehaviorSubject<string> = new BehaviorSubject('');
+  address$: BehaviorSubject<Address> = new BehaviorSubject(
+    new Address(
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      ''
+    )
+  );
 
   constructor(private mapService: MapService) {
     this.mapService
-      .getCoordinate()
+      .getInputCoordinate()
       .subscribe((coordinate: Coordinate) => this.reverseGeocode(coordinate));
   }
 
@@ -21,17 +52,20 @@ export class GeocodingService {
   }
 
   public reverseGeocode(coordinate: Coordinate) {
-    // TODO: Call geocoding API to actually resolve input
+    const authentication = ApiKeyManager.fromKey(
+      environment.reverseGeocodeAPIKey
+    );
 
-    this.cityName$.next('Berlin Mitte');
-    this.state$.next('Berlin');
+    const revGeocode = reverseGeocode([coordinate[0], coordinate[1]], {
+      authentication,
+    });
+    revGeocode.then(value => {
+      const address: Address = <Address>value.address;
+      this.address$.next(address);
+    });
   }
 
-  public getCityName(): BehaviorSubject<string> {
-    return this.cityName$;
-  }
-
-  public getStateName(): BehaviorSubject<string> {
-    return this.state$;
+  public getMarkerAddress(): BehaviorSubject<Address> {
+    return this.address$;
   }
 }
