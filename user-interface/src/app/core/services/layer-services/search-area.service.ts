3d78d9284ed941e66fffc2b15d17037bf5553a78
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-10-14 10:04:30 +0200

    Fix bug in search-area.service.ts

--- user-interface/src/app/core/services/layer-services/search-area.service.ts
+++ user-interface/src/app/core/services/layer-services/search-area.service.ts
@@ -8,7 +8,7 @@ import { Circle, Point } from 'ol/geom';
 import { Fill, Icon, Style } from 'ol/style';
 import { METERS_PER_UNIT } from 'ol/proj/Units';
 import { fromCircle, fromExtent } from 'ol/geom/Polygon';
-
+import { SearchArea } from '../../models/searcharea';
 
 @Injectable({
   providedIn: 'root',
@@ -25,7 +25,6 @@ export class SearchAreaService {
   }
 
   public initSearchArea(map: Map | undefined, view: View) {
-
     // layer with pin at search coordinate
     const pinFeature = new Feature({
       geometry: new Point(this.searchArea.coordinate),
@@ -57,26 +56,26 @@ export class SearchAreaService {
 
     // define the search area and extract its shape polygon
     const searchAreaPolygon = fromCircle(
-      new Circle(this.coordinate, radius),
-      64  // number of line elements that construct the circle
-    )
+      new Circle(this.searchArea.coordinate, radius),
+      64 // number of line elements that construct the circle
+    );
 
     // the layer should cover the whole view except for the search area
     let viewExtent = fromExtent(view.getProjection().getExtent());
 
     // cut out the search area from the layer by appending its linear ring shape to the extent polygon
-    const searchAreaLinearRing= searchAreaPolygon.getLinearRing(0)
-    viewExtent.appendLinearRing(searchAreaLinearRing!)
+    const searchAreaLinearRing = searchAreaPolygon.getLinearRing(0);
+    viewExtent.appendLinearRing(searchAreaLinearRing!);
 
     const searchAreaLayer = new VectorLayer({
       source: new VectorSource({
-        features: [new Feature({geometry: viewExtent})]
+        features: [new Feature({ geometry: viewExtent })],
       }),
       style: new Style({
-          fill: new Fill({
-            color:"rgba(0, 0, 0, 0.5)"
-        })
-      })
+        fill: new Fill({
+          color: 'rgba(0, 0, 0, 0.5)',
+        }),
+      }),
     });
 
     // add layers to map
